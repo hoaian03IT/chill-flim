5cb4dea2cf38c1a0197fe12baedf310a14673790
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-12-17 22:46:51 +0700

    film model

--- ChillFilm-BE/models/film.model.js
+++ ChillFilm-BE/models/film.model.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const filmSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
+        url: { type: String, required: true },
+        language: { type: String, required: true },
         genre: [{ type: String, required: true }],
         duration: { type: Number },
+        overview: { type: String, required: true },
         rate: [{ type: Number, required: true }],
     },
     {
